b453595cf300cfb11f14163f8351512183daa09c
Rakibul Hasan Asif <47877578+asif2508@users.noreply.github.com>
2022-04-03 23:29:52 +0600

    routes added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h5>THis is header</h5>
+        </div>
+    );
+};
+
+export default Header;<h5>THis is header</h5>
---
+++ src/components/Reviews/Reviews.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Reviews = () => {
+    return (
+        <div>
+            <h3>hello i am review</h3>
+        </div>
+    );
+};
+
+export default Reviews;
