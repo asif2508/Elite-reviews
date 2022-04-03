64cf8a9b85220ac3642f2c6664d58bd4de77dcc7
Rakibul Hasan Asif <47877578+asif2508@users.noreply.github.com>
2022-04-04 01:13:01 +0600

    header and home page half done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,26 @@
 import React from 'react';
-
+import {Navbar, Container, Nav} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 const Header = () => {
     return (
         <div>
-            <h5>THis is header</h5>
+            <Navbar bg="dark" variant="dark" expand="lg">
+            <Container>
+                <Navbar.Brand as={Link} to="/">Elite Reviews</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ms-auto">
+                <Nav.Link as={Link} to="/">Home</Nav.Link>
+                <Nav.Link as={Link} to="/reviews">Reviews</Nav.Link>
+                <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
+                <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
+                <Nav.Link as={Link} to="/about">Abouts</Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+            </Navbar>
         </div>
     );
 };
 
-export default Header;<h5>THis is header</h5>
+export default Header;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,32 @@
 import React from 'react';
-
+import { Col, Container, Row } from 'react-bootstrap';
+import Image from 'react-bootstrap/Image'
+import './Home.css';
 const Home = () => {
     return (
-        <div>
-          <h3>This is home</h3>  
+        <div className='home-container'>
+          <div className="header-container">
+            <Container>
+            <Row>
+                <Col xs={12} md={6} lg={6}>
+                    <div className="main-container">
+                    <h1>Choose The Right Product</h1>
+                    <h1>Follow Your Dream</h1>
+                    <p>Here you can see the best laptops ever.You can choose any laptop by the reviews given by the people. Best of luck. chase your dream laptop. Because we are suggesting you the world's best laptops based on the user experiances.
+                    </p>
+                    <a className='demo-link' href="/">Live Demo</a>
+                    </div>
+                </Col>
+                <Col xs={12} md={6} lg={6}>
+                    <Image src="https://media.istockphoto.com/photos/isolated-laptop-on-white-background-stock-photo-picture-id1294325987?b=1&k=20&m=1294325987&s=170667a&w=0&h=MUqxtcCsnCB5SGc9YxSyRkkvZ7vKR0RJEULeEpYgv1M=" fluid={true} />
+                </Col>
+                </Row>
+                </Container>
+          </div>
+        <div className="review-container">
+            <h1>Customer Reviews</h1>
+        </div>
+
         </div>
     );
 };
