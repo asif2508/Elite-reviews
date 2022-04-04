f7d925dc1dc4027215dd4e1d9b6f94358a4a2f80
Rakibul Hasan Asif <47877578+asif2508@users.noreply.github.com>
2022-04-04 23:56:53 +0600

    404 image added

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import {Image} from 'react-bootstrap';
+import './NotFound.css';
 const NotFound = () => {
     return (
-        <div>
-            <h3>404 not found</h3>
+        <div className='error-page'>
+            <Image src="https://img.freepik.com/free-vector/404-error-with-person-looking-concept-illustration_114360-7912.jpg?size=626&ext=jpg" fluid/>
         </div>
     );
 };
