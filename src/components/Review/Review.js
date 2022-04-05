df955bf47073dd83c7769c93e8447f31f5a52b5b
Rakibul Hasan Asif <47877578+asif2508@users.noreply.github.com>
2022-04-05 12:48:06 +0600

    reviews edited

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -11,7 +11,8 @@ const Review = (props) => {
             <h5  className='mt-3 ms-2 fw-bold'>{name}</h5>
             </div>
             <div className='d-flex justify-content-center align-items-start flex-column mt-2 ms-2'>
-                <p title={desc} className='review-para'><strong>review:</strong> {desc.length < 32 ? desc : desc.slice(0,32)+'...'}</p>
+                
+                <p className='description' title={desc}>{desc.length < 200 ? desc : desc.slice(0,200)+'...'}</p>
                 <p><strong>rating:</strong> {rating} stars</p>
             </div>
             </div>
