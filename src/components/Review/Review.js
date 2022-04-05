602f430a68033137b95907cbf6c2d67c9342635b
Rakibul Hasan Asif <47877578+asif2508@users.noreply.github.com>
2022-04-05 13:53:27 +0600

    rating stars added and conditional rendering

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Col, Image } from 'react-bootstrap';
 import './Review.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faStar } from '@fortawesome/free-solid-svg-icons';
 const Review = (props) => {
     const {name, picture, desc, rating} = props.review;
     return (
@@ -13,7 +15,36 @@ const Review = (props) => {
             <div className='d-flex justify-content-center align-items-start flex-column mt-2 ms-2'>
                 
                 <p className='description' title={desc}>{desc.length < 200 ? desc : desc.slice(0,200)+'...'}</p>
-                <p><strong>rating:</strong> {rating} stars</p>
+                {rating >= 3 && rating < 4 &&
+                <p>
+                    <strong>rating: </strong> 
+                    <FontAwesomeIcon className='star-color' icon={faStar}></FontAwesomeIcon>
+                    <FontAwesomeIcon className='star-color' icon={faStar}></FontAwesomeIcon>
+                    <FontAwesomeIcon className='star-color' icon={faStar}></FontAwesomeIcon>
+                    <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>
+                    <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>     
+                </p>
+                }
+                {rating >= 4 && rating < 5 &&
+                <p>
+                    <strong>rating: </strong> 
+                    <FontAwesomeIcon className='star-color' icon={faStar}></FontAwesomeIcon>
+                    <FontAwesomeIcon className='star-color' icon={faStar}></FontAwesomeIcon>
+                    <FontAwesomeIcon className='star-color' icon={faStar}></FontAwesomeIcon>
+                    <FontAwesomeIcon className='star-color' icon={faStar}></FontAwesomeIcon>
+                    <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>     
+                </p>
+                }
+                {rating ===5 &&
+                <p>
+                    <strong>rating: </strong> 
+                    <FontAwesomeIcon className='star-color' icon={faStar}></FontAwesomeIcon>
+                    <FontAwesomeIcon className='star-color' icon={faStar}></FontAwesomeIcon>
+                    <FontAwesomeIcon className='star-color' icon={faStar}></FontAwesomeIcon>
+                    <FontAwesomeIcon className='star-color' icon={faStar}></FontAwesomeIcon>
+                    <FontAwesomeIcon className='star-color' icon={faStar}></FontAwesomeIcon>     
+                </p>
+                }
             </div>
             </div>
         </Col>
